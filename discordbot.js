92892417ee03e3d63e9f00b233bd9cf3bc2a54b2
Tumble <tumblegamer@gmail.com>
2018-05-13 17:23:18 +0100

    added commands

--- discordbot.js
+++ discordbot.js
@@ -1,16 +1,105 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const DiscordOBJ = require('./discordobj');
+const DiscordServer = DiscordOBJ("server");
+const DiscordCommand = DiscordOBJ("command");
+
+var discordServers = [];
+var discordcommands = [];
+
+function registerServer(server){
+  discordServers.push(new DiscordServer(server.id,server.name));
+}
+function registerCommand(alias,cmdevent){
+  discordcommands.push(new DiscordCommand(alias,cmdevent));
+}
+
+function getServer(guildID, callback){
+  discordServers.forEach(server =>{
+    if (guildID===server.id) {
+      callback(server);
+    }
+  });
+}
+
+registerCommand("ping",function (message, param) {
+  message.channel.send('pong');
+});
+registerCommand("servers",function (message, param) {
+  message.channel.send("**Server List**")
+  discordServers.forEach((guild,index,array) => {
+    message.channel.send("* " + guild.name);
+  });
+});
+
+registerCommand("say", function (message, param) {
+  message.channel.send(param.join(" "));
+});
+
+registerCommand("commands", function (message, param) {
+  switch (param.shift()) {
+    case "list":
+      message.channel.send("**Commands**");
+      discordcommands.forEach((command,index,array) => {
+        message.channel.send("* " + command.alias[0]);
+      });
+      break;
+    case "add":
+      var commandAlias = param.shift();
+      message.channel.send("Adding command...");
+      switch (param.shift()) {
+        case "text":
+          var commandTextReplay = param.join(" ");
+          registerCommand(commandAlias, function (message, param) {
+            message.channel.send(commandTextReplay);
+          });
+          break;
+        default:
+
+      }
+      registerCommand()
+    break;
+    default:
+  }
+});
+
 client.on('ready', () => {
   console.log('I am ready!');
+  client.guilds.forEach(guild =>{
+    registerServer(guild);
+  });
 });
 
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
-  }
+  getServer(message.guild.id,function (server) {
+    if (message.content.charAt(0) == server.prefix) {
+      message.channel.startTyping();
+      var commandWorked = false;
+
+      var callback = function () {
+        if (!commandWorked) {
+          message.channel.send("Invalid command")
+        }
+        message.channel.stopTyping();
+      }
+      var itemsProcessed = 0;
+      discordcommands.forEach((command,index,array) => {
+        if (commandWorked) {
+          return;
+        }
+        commandWorked = commandWorked || command.execute(message);
+        itemsProcessed++;
+        if(itemsProcessed === array.length) {
+          callback();
+        }
+      });
+    }
+  });
+});
+
+client.on('guildCreate', guild => {
+  registerServer(guild);
 });
 
 module.exports = client;
